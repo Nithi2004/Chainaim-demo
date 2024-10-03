8f8c665ef62d7b90ecb6df3d312592a028a8bf9f
Nithi2004 <nnithish567@gmail.com>
2024-10-03 11:21:09 +0530

    modified home

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,58 +1,78 @@
 // src/app/components/Home.tsx
 import { Box, SimpleGrid, Text } from '@chakra-ui/react';
+import Link from 'next/link'; // Import the Link component
 
 const Home = () => {
   return (
     <Box>
       <SimpleGrid columns={2} spacing={8} mt={8}>
-        <Box
-          width="80%"
-          bgGradient="linear(to-r, orange.300, orange.400)"
-          height="200px"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          borderRadius="md"
-          mx="auto" // Center the boxes
-        >
-          <Text color="white" fontSize="lg">ZKPET Deep Composition Prover </Text>    
-        </Box>
-        <Box
-          width="80%"
-          bgGradient="linear(to-r, orange.300, orange.400)"
-          height="200px"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          borderRadius="md"
-          mx="auto" // Center the boxes
-        >
-          <Text color="white" fontSize="lg">ZKPET Domain Registry</Text>
-        </Box>
-        <Box
-          width="80%"
-          bgGradient="linear(to-r, orange.300, orange.400)"
-          height="200px"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          borderRadius="md"
-          mx="auto" // Center the boxes
-        >
-          <Text color="white" fontSize="lg">ZKPET Business Process Prover</Text>  
-        </Box>
-        <Box
-          width="80%"
-          bgGradient="linear(to-r, orange.300, orange.400)"
-          height="200px"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          borderRadius="md"
-          mx="auto" // Center the boxes
-        >
-          <Text color="white" fontSize="lg"> ZKPET Risk Model Prover</Text>
-        </Box>
+        <Link href="/deep-composition-prover" passHref>
+          <Box
+            as="a" // Ensures the Box behaves like a link
+            width="80%"
+            bgGradient="linear(to-r, orange.300, orange.400)"
+            height="200px"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            borderRadius="md"
+            mx="auto"
+            cursor="pointer" // Add cursor pointer to indicate it's clickable
+          >
+            <Text color="white" fontSize="lg">ZKPET Deep Composition Prover</Text>    
+          </Box>
+        </Link>
+
+        <Link href="/domain-registry" passHref>
+          <Box
+            as="a"
+            width="80%"
+            bgGradient="linear(to-r, orange.300, orange.400)"
+            height="200px"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            borderRadius="md"
+            mx="auto"
+            cursor="pointer"
+          >
+            <Text color="white" fontSize="lg">ZKPET Domain Registry</Text>
+          </Box>
+        </Link>
+
+        <Link href="/business-process-prover" passHref>
+          <Box
+            as="a"
+            width="80%"
+            bgGradient="linear(to-r, orange.300, orange.400)"
+            height="200px"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            borderRadius="md"
+            mx="auto"
+            cursor="pointer"
+          >
+            <Text color="white" fontSize="lg">ZKPET Business Process Prover</Text>  
+          </Box>
+        </Link>
+
+        <Link href="/risk-model-prover" passHref>
+          <Box
+            as="a"
+            width="80%"
+            bgGradient="linear(to-r, orange.300, orange.400)"
+            height="200px"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            borderRadius="md"
+            mx="auto"
+            cursor="pointer"
+          >
+            <Text color="white" fontSize="lg">ZKPET Risk Model Prover</Text>
+          </Box>
+        </Link>
       </SimpleGrid>
     </Box>
   );
